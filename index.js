f9cba6b46ead8f4442acc7519499c1ec0167ca2d
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-03-05 22:36:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -20,19 +20,20 @@ app.listen(PORT, '0.0.0.0', () => {
         apiKey: process.env.OPENAI_KEY,
     });
     client.on('messageCreate', async (msg) => {
-        if (msg.author.bot) return;
-        const response = await openai.chat.completions
-            .create({
-                model: 'gpt-3.5-turbo',
-                messages: [
-                    {
-                        role: 'user',
-                        content: msg.content,
-                    },
-                ],
-            })
-            .catch((e) => console.log(e));
-        msg.reply(response.choices[0].message.content);
+        if (!msg.author.bot && msg.mentions.has(client.user.id)) {
+            const response = await openai.chat.completions
+                .create({
+                    model: 'gpt-3.5-turbo',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: msg.content,
+                        },
+                    ],
+                })
+                .catch((e) => console.log(e));
+            msg.reply(response.choices[0].message.content);
+        }
     });
     client.login(process.env.TOKEN);
 });
