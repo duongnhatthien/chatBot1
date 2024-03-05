53d19d93b9d86f4098c22ce70514ef30a4ac5f66
accountgithub2003 <95201020+duongnhatthien@users.noreply.github.com>
2024-03-05 11:35:16 +0700

    thien
    second commit

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 require('dotenv/config');
 const { Client } = require('discord.js');
 const { OpenAI } = require('openai');
-const client = new Client({
-    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 10000; // Sử dụng cổng mặc định 10000 hoặc cổng được cung cấp bởi môi trường
+
+app.get('/', (req, res) => {
+    res.send('Server is running!');
 });
-client.on('ready', async () => {
-    await console.log('Bot is online');
+
+app.listen(PORT, '0.0.0.0', () => {
+    const client = new Client({
+        intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent'],
+    });
+    client.on('ready', async () => {
+        await console.log('Bot is online');
+    });
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_KEY,
+    });
+    client.on('messageCreate', async (msg) => {
+        if (msg.author.bot) return;
+        const response = await openai.chat.completions
+            .create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    {
+                        role: 'user',
+                        content: msg.content,
+                    },
+                ],
+            })
+            .catch((e) => console.log(e));
+        msg.reply(response.choices[0].message.content);
+    });
+    client.login(process.env.TOKEN);
 });
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY,
-});
-client.on('messageCreate', async (msg) => {
-    if (msg.author.bot) return;
-    const response = await openai.chat.completions
-        .create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'user',
-                    content: msg.content,
-                },
-            ],
-        })
-        .catch((e) => console.log(e));
-    msg.reply(response.choices[0].message.content);
-});
-client.login(process.env.TOKEN);
